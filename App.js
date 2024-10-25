ff8992009a827b99b083de0bad74f6638dda6ba9
itzAamir <amirpc190320@gmail.com>
2024-10-26 02:48:34 +0530

    minor fixes

--- App.js
+++ App.js
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
 
 // Basic route
 app.get("/", async (req, res) => {
-  await createTrailerForGenre("romcom");
-  res.send("Welcome to the Express CRUD API!");
+	await createTrailerForGenre("romcom");
+	res.send("Welcome to the Express CRUD API!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
